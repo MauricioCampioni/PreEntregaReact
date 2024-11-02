7fafa0ef01312ea09e81993f643fc1e474519de7
Mauricio Campioni <mauriciocampioni8@gmail.com>
2024-11-02 14:38:23 -0300

    Agregue un searchnavrbar.jsx y logica de busqueda en la barra

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import CartWidget from "../CartWidget/CartWidget";
 import {  Link } from "react-router-dom";
-import { StyleBanner} from "../../container/Styles";
+
 import { linkStyle, StyleNavBar } from "./StylesNavBar";
 const NavBar =()=>{
   const categorias =[{CategoriaId:1, nombreCategoria:"Hombre"},{CategoriaId:2, nombreCategoria:"Mujer"},{CategoriaId:3, nombreCategoria: "Niños"}];
 
     return (
       <header>
-        <nav style={{backgroundColor:"rgb(1,1,39)"}}>
+        <nav style={{backgroundColor:"rgb(0 ,0 ,0)"}}>
 
 
-            <img style={StyleBanner} src="/assets/Banner.jpg" alt="logo" />
+            
 
+            <h2 style={{color:"white", fontSize:"45px", fontFamily:"sans-serif"}}> Mi tienda </h2>
 
-            <h2 style={{color:"blue", fontSize:"45px", fontFamily:"sans-serif"}}>Boca <span style={{color:"yellow"}}>Store</span> </h2>
+            <div className='containerInput'>
+              <input
+              className='form-control'
+              
+              placeholder='Busqueda'
+              />
+            </div>
             
 
              <ul style={StyleNavBar}>
+
+
+            
                   <li key="Inicio">
                     <Link to={`/`} style={linkStyle} >Inicio</Link>
                   </li>
@@ -32,8 +42,10 @@ const NavBar =()=>{
                       </Link> 
                   
                     </li>
-                    
-                ))}
+                ))
+                
+                
+                }
                 <CartWidget/>
               </ul> 
               
